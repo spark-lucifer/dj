08f69fc0e18dc1e10de226eb6a396ed2a6067e49
Piyush chaudhary <piyushchy05@gmail.com>
2023-12-29 23:04:54 +0530

    registert

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views 
-
+from users import views as user_views
 urlpatterns = [
     path('', views.home,name='blog-home'),
+    path('register', user_views.register,name='register'),
     path('about', views.About,name='blog-about'),
 ]
